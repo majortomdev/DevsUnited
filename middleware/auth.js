974af917b1377c8d276e082a78b3a972a80d55b0
Joseph Kinahan <kinahanj25@gmail.com>
2020-05-11 19:41:32 +0100

    added middleware function 4  verifying the jsonwebtoken

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req,res,next) {
+    //to get the toekn from the header
+    const token = req.header('x-auth-token');
+    if(!token){
+        return res.status(401).json({ msg: 'No token, authorization denied'});
+            //id be wanting to verify the token...:
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        res.status(401).json({msg: 'Token is not valid'});
+    }
+
+    }
+}

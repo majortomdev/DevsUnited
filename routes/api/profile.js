dd3815b3e723edb510a455463d66dcf14a854f59
Joseph Kinahan <kinahanj25@gmail.com>
2020-05-13 19:14:31 +0100

    created route for GET method for this users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get('/whosyerman', (req,res) => res.send('Profile Route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   GET api/profile/me
+// @desc    get current user's profile
+// @access  private  
+router.get('/me', auth, async (req,res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user',
+        ['name', 'avatar']);
+
+        if(!profile){
+            return res.status(400).json({msg: 'There is no profile for this geezer'});
+        }
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('ServEr ErrOr');
+    }
+});
 
 module.exports= router;

549eba12c92b0a528953a9decea3d5151a14198f
Joseph Kinahan <kinahanj25@gmail.com>
2020-05-13 23:50:54 +0100

    added POST to profile api route for submitting a profile, or updating an existing

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -22,5 +23,71 @@ router.get('/me', auth, async (req,res) => {
         res.status(500).send('ServEr ErrOr');
     }
 });
+
+// @route   POST api/profile
+// @desc    create or update a user profile
+// @access  private  
+
+router.post('/',[ auth, [check('status','status is required')
+    .not()   //using array here as there are 2 middlewares, validator and auth
+    .isEmpty(), //and another array inside that cos im running 2 checks.....
+    check('skills','skills are required')
+    .not()
+    .isEmpty()
+    ]
+], 
+async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array() });//if  called will either say status is... 
+    } //   ....required, skills are required, or both
+    //here now i'll pull out all the attributes that are in the request body
+    const { company, website, location, bio, status, githubusername,
+        skills, youtube, facebook, twitter, instagram, linkedin } = req.body;
+
+    //must build a profile object
+    const profileProps = {};
+    profileProps.user = req.user.id;
+    if(company) profileProps.company = company;
+    if(website) profileProps.website = website;
+    if(location) profileProps.location = location;
+    if(bio) profileProps.bio = bio;
+    if(status) profileProps.status = status;
+    if(githubusername) profileProps.githubusername = githubusername;
+    if(skills) {
+        profileProps.skills = skills.split(',').map(skill => skill.trim());
+    }
+
+    //and now to build a social networks object
+    profileProps.social = {}
+    if(youtube) profileProps.social.youtube = youtube;
+    if(twitter) profileProps.social.twitter = twitter;
+    if(facebook) profileProps.social.facebook = facebook;
+    if(linkedin) profileProps.social.linkedin = linkedin;
+    if(instagram) profileProps.social.instagram = instagram;
+    
+    try {
+        let profile = await Profile.findOne({ user: req.user.id });
+        //iv used my Profile object to query DB for a profile with this current users id
+        if(profile) { //update
+            profile = await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileProps },
+                { new: true }
+                );
+
+            return res.json(profile);        
+        }
+        //or if theres not one found then we shall create one
+        profile = new Profile(profileProps);
+
+        await profile.save();
+        res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('sErVEr eRRor');
+    }
+})
 
 module.exports= router;

4c5085b89f52207a73073bb8a6c64a444d93c6d7
Joseph Kinahan <kinahanj25@gmail.com>
2020-05-15 00:54:02 +0100

    GET all profiles and GET profile by user Id requests

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -90,4 +90,40 @@ async (req, res) => {
     }
 })
 
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  public
+
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user',['name','avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("SerVER error<get all profiles method>");
+    }
+})
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  public
+
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user',['name','avatar']);
+        
+        if(!profile) return res.status(400).json({msg: 'There is noo profile for this user'});
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        if(err.name == 'CastError'){
+            //if(err.kind == 'ObjectId'){
+            return res.status(400).json({msg: 'There is no profile for this user'});
+        }//cant get this firing-> its for catching get requests with dud ids of unexpected length....
+        //changed it to err.name= 'CastError', worked...read it from error props when sent err to console...
+        res.status(500).send("SerVER error<get profile by id method>");
+    }
+})
+
 module.exports= router;

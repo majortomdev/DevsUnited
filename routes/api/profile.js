57a60f1855facfd7034c346a3b1559b39ec6d5b5
Joseph Kinahan <kinahanj25@gmail.com>
2020-05-20 23:01:26 +0100

    delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -176,4 +176,25 @@ check('from','From date is required').not().isEmpty()
     }
 })
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  private
+
+router.delete('/experience/:exp_id', auth, async (req,res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        //Get remove index
+        const removeIndex = profile.experience.map(item => item.id).indexOf
+        (req.params.exp_id);
+        console.log("removeIndex: "+removeIndex);
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('SerVER error in DELETE experience method');
+    }
+})
+
 module.exports= router;

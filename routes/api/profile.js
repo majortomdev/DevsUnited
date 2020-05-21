5bdba57788035cc98a4ddfa186ac261a9bfffd81
Joseph Kinahan <kinahanj25@gmail.com>
2020-05-22 00:32:03 +0100

    added a get method for getting users github repos..

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
@@ -248,6 +250,34 @@ router.delete('/education/:educ_id', auth, async (req,res) => {
     } catch (err) {
         console.error(err.message);
         res.status(500).send('SerVER error in DELETE education method');
+    }
+})
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from github
+// @access  public
+
+router.get('/github/:username',(req,res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}`,
+            // uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&
+            // client_id=${config.get('githubClientId')}&client_secret=${config.get(githubSecret)}`,
+            method: 'GET',
+            headers: { 'user-agent': 'node.js'}
+        };
+        //make a request object with a callback with a possible error, a response, amd a body.....
+        request(options, (error, response, body) => {
+            if(error) console.log(error);
+            if(response.statusCode !== 200 ){
+                return res.status(404).json({msg: 'No Github profile found '});
+            }
+
+            res.json(JSON.parse(body));
+        });
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error get github...');
     }
 })
 
